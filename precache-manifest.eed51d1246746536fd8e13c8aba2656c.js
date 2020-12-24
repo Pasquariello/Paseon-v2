61c36348b91300cb5a71f60116c340fe6d62f57b
Taylor Pasquariello <taylorpasquariello@Taylors-MacBook-Pro.local>
2020-12-23 20:17:01 -0700

    Updates

---
+++ precache-manifest.eed51d1246746536fd8e13c8aba2656c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b377b84b5d1142fce266dbb5569c3b6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fab32673ee12bb335fd",
+    "url": "/static/css/2.48a77e21.chunk.css"
+  },
+  {
+    "revision": "9fab32673ee12bb335fd",
+    "url": "/static/js/2.c68b4961.chunk.js"
+  },
+  {
+    "revision": "01afbb95eccb39bee35af8cf2aeaa1e3",
+    "url": "/static/js/2.c68b4961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0526f7acbd2d948faf81",
+    "url": "/static/js/3.6e9f6225.chunk.js"
+  },
+  {
+    "revision": "5b6a3f1ca6e193597e4d49c7a42851aa",
+    "url": "/static/js/3.6e9f6225.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0796d94480fe27bf3ff",
+    "url": "/static/js/4.306a8441.chunk.js"
+  },
+  {
+    "revision": "7f9bc9bd6c61fdbe6be8",
+    "url": "/static/js/5.a2d978d3.chunk.js"
+  },
+  {
+    "revision": "c8237f95175743d9459c",
+    "url": "/static/js/6.670f961d.chunk.js"
+  },
+  {
+    "revision": "6e0c54c4a79812bed530",
+    "url": "/static/js/7.f8dc7c62.chunk.js"
+  },
+  {
+    "revision": "5b88d448c9f20d800ad3",
+    "url": "/static/js/8.847398ec.chunk.js"
+  },
+  {
+    "revision": "d821503df3db6a5988f3",
+    "url": "/static/js/main.cd6bb65e.chunk.js"
+  },
+  {
+    "revision": "98f8666732914c84d6c4",
+    "url": "/static/js/runtime-main.db5449c7.js"
+  }
+]);
